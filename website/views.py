e36a00b66bd68c78a8bc334bfa97f286c07a9b2e
Jacob Fogle <48457739+iBologna@users.noreply.github.com>
2023-04-01 16:10:23 -0500

    Added fuel quote history page

--- website/views.py
+++ website/views.py
@@ -17,14 +17,20 @@ def home():
         address = current_user.profile[0].address1 #need to display this on html? 
         date = request.form.get('date') #currently a string cant get it to be "date" data type
               #store pricing per gallon/display on html 
-        price_per = 3 
-        cost = gallons * price_per 
+        price_to_gallon = 3 
+        cost = gallons * price_to_gallon
         quote = Quote(gallons = gallons, address = address, date = date, cost = cost, 
         user_id =current_user.id)
         db.session.add(quote)
         db.session.commit()
-        return redirect(url_for('views.home')) #needs to point to history.html once that works 
-    return render_template("quote.html", user=current_user)
+        return render_template("quote.html", user=current_user, cost = cost, price_to_gallon = price_to_gallon, gallons = gallons, date = date) #needs to point to history.html once that works 
+    return render_template("quote.html", user=current_user, cost = 0, price_to_gallon = 0, gallons = 0, date = "2023-01-01")
+
+@views.route('/history', methods=['GET', 'POST'])
+@login_required
+def history():
+    quotes = Quote.query.filter(Quote.user_id == current_user.id).all()
+    return render_template("history.html", user=current_user, quotes = quotes)
 
 
 
